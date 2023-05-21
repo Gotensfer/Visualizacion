1f66ba84a2c20793db9a95077931f88aebab9326
Julian Arroyave <jarroyavep@outlook.com>
2023-05-21 16:34:05 -0500

    Correccioncitas

--- Assets/Scripts/FillController.cs
+++ Assets/Scripts/FillController.cs
@@ -7,20 +7,20 @@ using DG.Tweening;
 public class FillController : MonoBehaviour
 {
     [SerializeField] float Time;
-    [SerializeField] GameObject text;
+    [SerializeField] GameObject CloseButton;
     private Image imageToFill;
 
 
     void OnEnable()
     {
         imageToFill = GetComponent<Image>();
-        imageToFill.DOFillAmount(1, Time).SetDelay(0.5f).OnComplete(() => { text.SetActive(true); });
+        imageToFill.DOFillAmount(1, Time).SetDelay(0.5f).OnComplete(() => { CloseButton.SetActive(true); });
     }
 
     void OnDisable()
     {
         imageToFill.DORestart();
-        text.SetActive(false);
+        CloseButton.SetActive(false);
         imageToFill.fillAmount = 0;
     }    
 }
